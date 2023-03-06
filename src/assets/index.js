519d1cace48150250d446f4720558f66449fe0fe
Vaishnav Vijayan <vaishnavtdy@gmail.com>
2023-03-06 18:03:38 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,6 @@ import Home1 from './Homepage-Model-3-Desktop-LHD.jpg'
 import Home2 from './Homepage-Model-S-Desktop-LHD.jpg'
 import Home3 from './Homepage-Model-X-Desktop-LHD.jpg'
 
+
+
 export { Logo, Home1, Home2, Home3 }
